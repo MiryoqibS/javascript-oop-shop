33f60d21c3c756b1dd1792bd9466afab6de10791
Miryoqib Sobirov <miryoqibcoder@gmail.com>
2025-06-06 17:48:56 +0500

    style(catalog): добавлена адаптивность для catalog

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -17,36 +17,99 @@ export class Product {
     }
 
     render() {
+        // Link for svg elements
+        const SVG_NS = "http://www.w3.org/2000/svg";
+        const XLINK_NS = "http://www.w3.org/1999/xlink";
+
         const productElement = document.createElement("article");
         productElement.className = "catalog-section__product product";
-        productElement.innerHTML = `
-            <div class="product__header">
-                <img src="${this.image}" class="product__image"/>
-                ${this.isOriginal ? ` 
-                <span class="product__original">
-                    <svg width="12" height="12">
-                        <use href="${sprites}#icon-verify"/>
-                    </svg>
-                    Оригинал
-                </span>` : ""}
-            </div>
-            <div class="product__body">
-                <p class="product__price">${this.price} ₽</p>
-                <h3 class="product__title">${this.title}</h3>
-                <div class="product__rating">
-                    <svg width="12" height="12">
-                        <use href="${sprites}#icon-star"/>
-                    </svg>
-                    <p>${this.rating}</p>
-                </div>
-                <button 
-                    class="product__button button button--secondary button--outline">
-                    ${this.storage.getFromStorage("cart").includes(this.id) ? "Удалить с корзины" : "В корзину"}</button>
-            </div>
-        `;
 
-        const button = productElement.querySelector(".product__button");
-        button.addEventListener("click", (event) => this.addToCart(event));
+        // Product header
+        const productHeader = document.createElement("div");
+        productHeader.className = "product__header";
+
+        // Product image
+        const productImage = document.createElement("img");
+        productImage.className = "product__image";
+        productImage.src = this.image;
+        productHeader.append(productImage);
+
+        productElement.append(productHeader);
+
+        // Product verify
+        if (this.isOriginal) {
+            const productVerify = document.createElement("span");
+            productVerify.className = "product__original";
+
+            const verifyIcon = document.createElementNS(SVG_NS, "svg");
+            verifyIcon.setAttribute("width", "12");
+            verifyIcon.setAttribute("height", "12");
+
+            const verifyIconUse = document.createElementNS(SVG_NS, "use");
+            verifyIconUse.setAttributeNS(XLINK_NS, "xlink:href", `${sprites}#icon-verify`);
+            verifyIcon.appendChild(verifyIconUse);
+
+            const productVerifyText = document.createElement("p");
+            productVerifyText.innerText = "оригинал";
+
+            productVerify.append(verifyIcon, productVerifyText);
+
+            productHeader.append(productVerify);
+        };
+
+        // Product body
+        const productBody = document.createElement("div");
+        productBody.className = "product__body";
+
+        // Product price
+        const productPrice = document.createElement("p");
+        productPrice.className = "product__price";
+        productPrice.innerText = `${this.price}₽`;
+
+        // Product title
+        const productTitle = document.createElement("h3");
+        productTitle.className = "product__title";
+        productTitle.innerText = this.title;
+
+        // Product rating
+        const productRating = document.createElement("div");
+        productRating.className = "product__rating";
+
+        const starIcon = document.createElementNS(SVG_NS, "svg");
+        starIcon.setAttribute("width", "12");
+        starIcon.setAttribute("height", "12");
+
+        const starIconUse = document.createElementNS(SVG_NS, "use");
+        starIconUse.setAttributeNS(XLINK_NS, "xlink:href", `${sprites}#icon-star`);
+        starIcon.append(starIconUse);
+        productRating.append(starIcon);
+
+        const productRatingText = document.createElement("p");
+        productRatingText.innerText = this.rating;
+        productRating.append(productRatingText);
+
+
+        // Product button
+        let inCart = this.storage.getFromStorage("cart").includes(this.id);
+        const productButton = document.createElement("button");
+        productButton.classList.add("button");
+        productButton.classList.add("product__button");
+        productButton.classList.add("outline");
+
+        if (inCart) {
+            productButton.classList.add("button--primary");
+            productButton.innerText = "Удалить с корзины";
+        } else {
+            productButton.classList.add("button--secondary");
+            productButton.innerText = "В корзину";
+        };
+
+        productButton.addEventListener("click", (event) => this.addToCart(event));
+
+        productBody.append(productPrice, productTitle, productRating, productButton);
+
+        // Add header, body elements
+        productElement.append(productHeader, productBody);
 
         return productElement;
     }
@@ -60,14 +123,19 @@ export class Product {
         };
 
         if (!prevProducts.includes(this.id)) {
+            // Add button
             prevProducts.push(this.id);
             this.storage.saveToStorage("cart", prevProducts);
-            event.target.innerHTML = "Удалить с корзины"
+            event.target.classList.remove("button--secondary");
+            event.target.classList.add("button--primary");
+            event.target.innerText = "Удалить с корзины"
         } else {
+            // Remove button
             const newProducts = prevProducts.filter(id => id !== this.id);
             this.storage.saveToStorage("cart", newProducts);
-            event.target.innerHTML = "В корзину"
+            event.target.classList.remove("button--primary");
+            event.target.classList.add("button--secondary");
+            event.target.innerText = "В корзину";
         };
-
     }
 }
